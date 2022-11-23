3d6111eec95ee63980474ab1c2af8a42199e891b
alexsanjoseph <alexsanjoseph@gmail.com>
2022-11-23 15:15:26 +0530

    Permutations done

--- golang/kyu4/permutations.go
+++ golang/kyu4/permutations.go
@@ -1,5 +1,34 @@
 package kata
 
+import "strings"
+
+func removeIndex(s []string, index int) []string {
+	ret := make([]string, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
 func Permutations(s string) []string {
-	return []string{}
+
+	splitString := strings.Split(s, "")
+	if len(splitString) == 1 {
+		return splitString
+	}
+
+	uniqueCombinations := map[string]bool{}
+
+	for i, x := range splitString {
+		newString := removeIndex(splitString, i)
+		allPermutations := Permutations(strings.Join(newString, ""))
+		for _, single := range allPermutations {
+			uniqueCombinations[x+single] = true
+		}
+	}
+
+	output := []string{}
+	for key := range uniqueCombinations {
+		output = append(output, key)
+	}
+
+	return output
 }
